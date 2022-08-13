4e57ae3896b1d1af1d9ffeedc3bd97acd6810fa5
Fatih2Sengul <111070193+Fatih2Sengul@users.noreply.github.com>
2022-08-13 14:10:34 +0300

    Model: Product ProductCategory Repository: ProductRepository ProductCategoryRepository

--- src/main/java/com/fsengul/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/fsengul/ecommerce/config/MyDataRestConfig.java
@@ -27,7 +27,7 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         entityManager = theEntityManager;
     }
 
-
+//fsengul
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 
